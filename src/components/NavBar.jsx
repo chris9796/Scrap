45605065798ad0d9fcfe5d0461eb530c50f0b219
PCHEO <chirs9796@naver.com>
2023-12-17 23:58:14 +0900

    lastpc

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,6 +4,8 @@ import NavLogo from './NavLogo';
 import NavButton from './NavButton';
 import {Link} from "react-router-dom"
 function NavBar(){
+//어떻게 하면 우리가 계속 재활용 할수 있을까? 이게 가장중요한데 어떻게 하지
+//그냥 우리는 따로따로 나누자 이거 너무 묶엇어 넘 ㅄ임 ㄹㅇ
 
 
     return(
--- src/components/SectionInput.jsx
+++ src/components/SectionInput.jsx
@@ -3,6 +3,7 @@ import "../components_css/SectionInput.css"
 function SectionInput(props){
     
     return(
+        
         <input type="text" placeholder={props.name} className="SectionInput"/>
 
     );
